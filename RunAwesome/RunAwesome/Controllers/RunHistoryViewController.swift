8cc5aaf539241e05b4dab09ac33a73e0b960eab1
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-04-24 17:35:35 +0200

    Configure TabBarVIewContrillers

---
+++ RunAwesome/RunAwesome/Controllers/RunHistoryViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MyRunsViewController.swift
+//  RunAwesome
+//
+//  Created by Eslam Ali  on 24/04/2022.
+//
+
+import UIKit
+
+class RunHistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RunAwesome/RunAwesome/Controllers/TabBarViewController.swift
@@ -0,0 +1,40 @@
+//
+//  TabBarViewController.swift
+//  RunAwesome
+//
+//  Created by Eslam Ali  on 24/04/2022.
+//
+
+import UIKit
+
+final class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        tabBar.tintColor = .label
+       
+        let vc1 = HomeViewController()
+        let vc2 = RunHistoryViewController()
+        
+        vc1.tabBarItem.image = UIImage(systemName: "house")
+        vc1.tabBarItem.selectedImage = UIImage(systemName: "house.fill")
+        
+        
+        vc2.tabBarItem.image = UIImage(systemName: "clock")
+        vc2.tabBarItem.selectedImage = UIImage(systemName: "clock.fill")
+        
+        
+        
+        
+    setViewControllers([vc1, vc2], animated: true)
+        
+        
+        
+        
+    }
+    
+
+ 
+
+}

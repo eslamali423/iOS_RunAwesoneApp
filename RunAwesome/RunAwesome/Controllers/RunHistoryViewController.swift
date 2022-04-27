7f30d664b101e815bb98dca4f320d3ae545e435e
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-04-27 20:26:10 +0200

    Set Layout for RunHistoryViewController and configure Constraints

--- RunAwesome/RunAwesome/Controllers/RunHistoryViewController.swift
+++ RunAwesome/RunAwesome/Controllers/RunHistoryViewController.swift
@@ -9,23 +9,106 @@ import UIKit
 
 class RunHistoryViewController: BaseViewController {
 
+    //MARK:- Vars
+  
+    
+    private let titleView : UIView = {
+       let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        view.alpha = 0.7
+        return view
+    }()
+    
+    
+    private let topLabel : UILabel = {
+        let label = UILabel()
+        label.text = "History"
+        label.alpha = 1
+        label.font = .systemFont(ofSize: 32, weight: .bold)
+        label.textAlignment = .center
+        label.textColor = .darkGray
+        label.translatesAutoresizingMaskIntoConstraints = false
+    
+        
+        return label
+    }()
+    
+    private let tableView : UITableView = {
+        let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.backgroundColor = .clear
+        table.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        table.separatorColor = .white
+        
+        return table
+        
+        
+    }()
+    
+  
+    
+    //MARK:- Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
+        setLayout()
+        configureConstraints()
+        tableView.tableFooterView = UIView()
+        tableView.delegate = self
+        tableView.dataSource = self
 
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    //MARK:- Set Layouts
+    private func setLayout(){
+        titleView.addSubview(topLabel)
+        view.addSubview(titleView)
+        view.addSubview(tableView)
+    
+    }
+    //MARK:- Configure Constraints
+    private func configureConstraints(){
+        
+        NSLayoutConstraint.activate([
+        
+            // Title Lable Constraints
+            titleView.topAnchor.constraint(equalTo: view.topAnchor),
+            titleView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            titleView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            titleView.heightAnchor.constraint(equalToConstant: 100),
+            
+            topLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            topLabel.topAnchor.constraint(equalTo: titleView.safeAreaLayoutGuide.topAnchor, constant: 8),
+            
+            
+            tableView.topAnchor.constraint(equalTo: titleView.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+        ])
+        
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+
+//MARK:- Extension For tableView
+extension RunHistoryViewController : UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell =  tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+       
+        return cell
+    }
+   
 }
